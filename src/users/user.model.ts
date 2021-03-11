97e23da322ad7a709d5751c6cf32ba284e4cb00a
Alex <alexandra.nguyen@epitech.eu>
2021-03-11 13:47:44 +0100

    password removed & reole added

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -13,11 +13,19 @@ export class User {
   @Prop({ required: true })
   email: string;
 
-  @Prop({ required: true })
-  password: string;
+  // * removed because firebase don't provide access to the password
+  // @Prop({ required: true })
+  // password: string;
 
   @Prop({ required: true })
   firebaseUid: string;
+
+  @Prop({
+    required: true,
+    default: 'STUDENT',
+    enum: ['STUDENT', 'TEACHER', 'ADMIN']
+  })
+  role: string
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
